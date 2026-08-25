e832260e16e22bca6996caa635a18faace87bb07
Bhuvan <bhuvanchandranalabothu@gmail.com>
2026-08-25 17:49:06 +0530

    assignment problems

---
+++ control flow/assignment_problems/SumOfNaturalNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfNaturalNumbers {
+    static void sumOfNaturalNumbers(int n) {
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sumOfNaturalNumbers(n);
+        sc.close();
+    }
+}
